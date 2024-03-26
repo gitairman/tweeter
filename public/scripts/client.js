830acda13fd323157fd212914510a18a90b50920
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-26 08:59:01 -0700

    added creatTweetElement and renderTweets functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = (tweetData) => {
+  const { user, content, created_at } = tweetData;
+  return `
+      <article class="tweet">
+        <header>
+          <div class="tweet_author">
+            <img src=${user.avatars} alt="${user.name}'s avatar"/>
+            <span>${user.name}</span>
+          </div>
+          <span class="handle"> ${user.handle} </span>
+        </header>
+        <section class="tweet_text">
+          <p>${content.text}</p>
+        </section>
+        <footer>
+          <time>${created_at}</time>
+          <div class="icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-regular fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+`;
+};
+
+const renderTweets = (tweets) => {
+  tweets.forEach((tweet) => {
+    $('.container').append(createTweetElement(tweet));
+  });
+};
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac',
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants',
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd',
+    },
+    content: {
+      text: 'Je pense , donc je suis',
+    },
+    created_at: 1461113959088,
+  },
+];
+
+$(document).ready(function () {
+  renderTweets(data);
+});
